cb1199c5c162c0d897798a5fd3a02eeb9dadbde1
fylora <fylora189@gmail.com>
2024-02-22 19:50:21 +0200

    Initial commit

---
+++ src/main/kotlin/com/fylora/Application.kt
@@ -0,0 +1,16 @@
+package com.fylora
+
+import com.fylora.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureDatabases()
+    configureSecurity()
+    configureRouting()
+}
